fc6f5fc34f5e972ca739c601f4fee1a3050faf50
Iryna.Hrychyna <iryna.hrychyna@cshark.com>
2023-06-19 13:00:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Playwright – Praktyczne wprowadzenie do testów automatycznych
+
+## Playwright tips&tricks
+
+- test.only - runs a single tests
+- blur - unset focus from an element
+
+## Visual Studio Code howto
+- File -> Auto-save
+
+### Shortcuts
+- Shift-Alt-/ - format document
---
+++ tests/login.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect, selectors } from '@playwright/test';
+
+test.describe('login tests', () => {
+  test('positive login test with valid credentials', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('testerLO');
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('password-input').fill('qwerty12');
+    await page.getByTestId('login-button').click();
+    await page.getByTestId('user-name').click();
+    await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
+  });
+
+  test('negative login test with invalid username', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('tester');
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('password-input').fill('qwerty12');
+
+    await expect(page.getByTestId('login-button')).toHaveAttribute('disabled', '');
+    await expect(page.getByTestId('error-login-id')).toHaveText('identyfikator ma min. 8 znaków');
+  });
+
+  test('negative login test with invalid password', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('testerLO');
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('password-input').fill('qwerty');
+    await page.getByTestId('password-input').blur();
+    
+    await expect(page.getByTestId('login-button')).toHaveAttribute('disabled', '');
+    await expect(page.getByTestId('error-login-password')).toHaveText('hasło ma min. 8 znaków');
+  });
+
+});
